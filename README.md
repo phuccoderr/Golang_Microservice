650baa92ea8b53c1f03b2112719d35973a4d4599
phuccoderr <phuctapcode@gmail.com>
2024-07-19 01:55:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Install Chi
+
+```
+go get github.com/go-chi/chi/v5
+go get github.com/go-chi/chi/v5/middleware
+go get github.com/go-chi/cors
+```
+
+# Install PostgreSql
+
+```
+go get github.com/jackc/pgconn
+go get github.com/jackc/pgx/v4
+go get github.com/jackc/pgx/stdlib
+```
+
+# Install MongoDB
+
+~~~
+go get go.mongodb.org/mongo-driver/mongo
+go get go.mongodb.org/mongo-driver/mongo/options
+~~~
+
+# Docker
+
+```
+docker exec -it <postgres-container-id-or-name> psql -U postgres
+\c users
+```
---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"log-service/data"
+	"net/http"
+	"time"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+	Models data.Models
+}
+
+func main() {
+	// connect to mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+	// create context in order to disconnect
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+	// start web server
+	//go app.server()
+	log.Println("Starting web server on " + webPort)
+	srv := http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func (app *Config) server() {
+	srv := http.Server{
+		Addr: fmt.Sprintf(":%s", webPort),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func connectToMongo() (*mongo.Client, error) {
+
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	//connect
+	connect, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting to MongoDB: ", err)
+		return nil, err
+	}
+
+	log.Println("Connected to MongoDB")
+
+	return connect, nil
+}
